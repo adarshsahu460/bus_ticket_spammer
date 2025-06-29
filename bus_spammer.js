8621fce3c1b61f65d61af56b6c4e7e67a66e876c
adarshsahu460 <adarshsahu460@gmail.com>
2025-06-29 11:20:08 +0530

    first commit

---
+++ bus_spammer.js
@@ -0,0 +1,32 @@
+// bus_spammer.js
+// Continuously sends requests to two APIs every 5 seconds without waiting for responses
+
+const fs = require('fs');
+const axios = require('axios');
+
+const req1 = JSON.parse(fs.readFileSync('./req1.json', 'utf8'));
+const req2 = JSON.parse(fs.readFileSync('./req2.json', 'utf8'));
+
+const endpoints = [
+  {
+    url: 'https://www.konduskarbus.in/api/cms_booking_engine.json',
+    data: req1
+  },
+  {
+    url: 'https://www.ashokatravels.net/api/cms_booking_engine.json',
+    data: req2
+  }
+];
+
+function fireAndForget(url, data) {
+  axios.post(url, data).catch(() => {}); // Ignore errors and responses
+}
+
+setInterval(() => {
+  endpoints.forEach(({ url, data }) => {
+    fireAndForget(url, data);
+    console.log(`[${new Date().toISOString()}] Sent request to ${url}`);
+  });
+}, 5000);
+
+console.log('Bus spammer started. Sending requests every 5 seconds...');
